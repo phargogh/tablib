220987f34ea333a917d5cd30cb10ac1ea31f68b7
James Douglass <jamesdouglassusa@gmail.com>
2014-06-13 11:44:33 -0700

    Switching test order of formats
    Putting dbf format testing ahead of TSV.  In some of my tests with
    numeric DBF files, I encountered an issue where the ASCII horizontal
    tab character (0x09) would appear in a numeric DBF.  Because of the
    order of tabular format imports, though, format detection would
    recognize it as a TSV and not as a DBF.

--- tablib/formats/__init__.py
+++ tablib/formats/__init__.py
@@ -13,4 +13,4 @@ from . import _xlsx as xlsx
 from . import _ods as ods
 from . import _dbf as dbf
 
-available = (json, xls, yaml, csv, tsv, html, xlsx, ods, dbf)
+available = (json, xls, yaml, csv, dbf, tsv, html, xlsx, ods)
